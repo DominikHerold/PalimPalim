75c1bffbdcd32aad186af484cc8f6572fa02a5f2
Dominik Herold <d.herold@cid.com>
2018-10-05 12:32:46 +0200

    init commit of PalimPalim

---
+++ PalimPalim/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+using HtmlAgilityPack;
+
+namespace PalimPalim
+{
+    public class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# PalimPalim
+
+Sends Pushnotifications if there are new ¶ [Palai](https://palai.org/)
+
+[PalimPalim](https://www.youtube.com/watch?v=iZNOk7upy7A&feature=youtu.be&t=53)
