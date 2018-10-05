999528c13d09d993a8a3761b464f9b4778acdef3
Dominik Herold <d.herold@cid.com>
2018-10-05 14:20:02 +0200

    download content

---
+++ PalimPalim/ElapsedEventHandler.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace PalimPalim
+{
+    public delegate void ElapsedEventHandler(object sender, EventArgs e);
+}
--- PalimPalim/Program.cs
+++ PalimPalim/Program.cs
@@ -1,17 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
-
-using HtmlAgilityPack;
 
 namespace PalimPalim
 {
     public class Program
     {
+        private static Timer Timer;
+
+        private const string Cookie = "foo"; // ToDo change
+        private const string IncomesUrl = "/users/42/basic_incomes"; // ToDo change
+        private const string PushoverKey = "token=foo&user=bar&message=PalimPalim"; // ToDo change
+
         private static void Main()
         {
+            Console.WriteLine("Start");
+
+            CheckPalai();
+
+            Timer = new Timer((int)TimeSpan.FromHours(5).TotalMilliseconds, false);
+            Timer.Elapsed += CheckPalai;
+
+            CheckPalai(null, null);
+
+            Console.ReadLine();
+            Timer.Dispose();
+        }
+
+        private static string DownloadString(string address)
+        {
+            using (var webClient = new HttpClient())
+            {
+                webClient.DefaultRequestHeaders.Add(nameof(Cookie), Cookie);
+                var data = webClient.GetAsync(address).GetAwaiter().GetResult().Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                return data;
+            }
+        }
+
+        private static void SendToPushoverApi()
+        {
+            var client = new HttpClient();
+            var toSend = string.Format($"{PushoverKey}&title=PalimPalim&url=https://palai.org{IncomesUrl}");
+            var now = DateTime.Now;
+            if (now.Hour >= 22 || now.Hour < 7)
+                toSend = string.Format("{0}&sound=none", toSend);
+
+            client.PostAsync("https://api.pushover.net/1/messages.json", new StringContent(toSend, Encoding.UTF8, "application/x-www-form-urlencoded")).GetAwaiter().GetResult();
+        }
+
+        private static void CheckPalai()
+        {
+            var content = DownloadString($"https://palai.org{IncomesUrl}");
+            if (content.Contains($@"<form class=""button_to"" method=""post"" action=""{IncomesUrl}"">"))
+                SendToPushoverApi();
+        }
+
+        private static void CheckPalai(object sender, EventArgs e)
+        {
+            try
+            {
+                CheckPalai();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Timer.Start();
+            }
         }
     }
 }
