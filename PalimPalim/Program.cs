79365222bc872a98a38e3c03ae8aa4b145dd51dd
Dominik Herold <d.herold@cid.biz>
2018-10-10 22:55:12 +0200

    claims Palai

--- PalimPalim/Program.cs
+++ PalimPalim/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+
+using HtmlAgilityPack;
 
 namespace PalimPalim
 {
@@ -10,6 +14,7 @@ namespace PalimPalim
 
         private const string Cookie = "foo"; // ToDo change
         private const string IncomesUrl = "/users/42/basic_incomes"; // ToDo change
+        private const string TransfersUrl = "/users/42/transfers"; // ToDo change
         private const string PushoverKey = "token=foo&user=bar&message=PalimPalim"; // ToDo change
 
         private static void Main()
@@ -35,10 +40,10 @@ namespace PalimPalim
             }
         }
 
-        private static void SendToPushoverApi()
+        private static void SendToPushoverApi(string balance)
         {
             var client = new HttpClient();
-            var toSend = string.Format($"{PushoverKey}&title=PalimPalim&url=https://palai.org{IncomesUrl}");
+            var toSend = string.Format($"{PushoverKey} {balance}&title=PalimPalim&url=https://palai.org{IncomesUrl}");
             var now = DateTime.Now;
             if (now.Hour >= 22 || now.Hour < 7)
                 toSend = string.Format("{0}&sound=none", toSend);
@@ -50,7 +55,27 @@ namespace PalimPalim
         {
             var content = DownloadString($"https://palai.org{IncomesUrl}");
             if (content.Contains($@"<form class=""button_to"" method=""post"" action=""{IncomesUrl}"">"))
-                SendToPushoverApi();
+            {
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(content);
+                var formNode = htmlDoc.DocumentNode.SelectNodes($"//form[@action='{IncomesUrl}']").Single();
+                var authNode = formNode.SelectSingleNode(".//input[@name='authenticity_token']");
+                var authToken = authNode.GetAttributeValue("value", "notset");
+                authToken = WebUtility.UrlEncode(authToken);
+
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add(nameof(Cookie), Cookie);
+                var toSend = $"authenticity_token={authToken}";
+                client.PostAsync($"https://palai.org{IncomesUrl}", new StringContent(toSend, Encoding.UTF8, "application/x-www-form-urlencoded")).GetAwaiter().GetResult();
+
+                var transferData = DownloadString($"https://palai.org{TransfersUrl}");
+                htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(transferData);
+                var balanceNode = htmlDoc.DocumentNode.SelectNodes("//td[@class='current-balance autohide']").Single();
+                var balance = balanceNode.InnerText.Trim();
+
+                SendToPushoverApi(balance);
+            }
         }
 
         private static void CheckPalai(object sender, EventArgs e)
--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 # PalimPalim
 
-Sends Pushnotifications if there are new ¶ [Palai](https://palai.org/)
+Checks if there are new ¶ [Palai](https://palai.org/) and tries to claim. Afterwards it sends a Pushnotification with current balance.
 
 [PalimPalim](https://www.youtube.com/watch?v=iZNOk7upy7A&feature=youtu.be&t=53)
